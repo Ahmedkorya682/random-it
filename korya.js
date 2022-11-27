324c03e914ac9cc1b47655bcb98e7e08ccfd2800
Ahmedkorya682 <111289577+Ahmedkorya682@users.noreply.github.com>
2022-11-27 23:11:16 +0200

    Add files via upload

---
+++ korya.js
@@ -0,0 +1,5 @@
+function korya(){
+    var x =["“I'm selfish, impatient and a little insecure. I make mistakesI am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.” ","“Be yourself; everyone else is already taken.”","“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”","“A room without books is like a body without a soul.”"]
+    var y=Math.floor(Math.random()*x.length)
+    document.getElementById("bta").innerHTML=x[y]
+    }
